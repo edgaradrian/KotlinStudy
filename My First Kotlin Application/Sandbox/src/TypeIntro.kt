6449d54221e0236281b3eea39c5994f60cc75dbc
EDGAR LAPTOP-LENOVO <addickto@hotmail.com>
2019-04-05 11:25:26 -0600

    Listing 2.6 Adding a playerName val

--- My First Kotlin Application/Sandbox/src/TypeIntro.kt
+++ My First Kotlin Application/Sandbox/src/TypeIntro.kt
@@ -1,5 +1,7 @@
 fun main(args: Array<String>){
+    val playerName :String = "addick"
     var experiencePoints: Int = 5
     experiencePoints += 5
     println(experiencePoints)
+    println(playerName)
 }//main
