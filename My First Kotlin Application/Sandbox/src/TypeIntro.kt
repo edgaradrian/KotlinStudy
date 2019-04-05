cb6c5ca9b294dd194aa48bc3f9e7f3cf3c8b9dbc
EDGAR LAPTOP-LENOVO <addickto@hotmail.com>
2019-04-05 12:30:26 -0600

    Listing 2.7 Removing explicit type specification

--- My First Kotlin Application/Sandbox/src/TypeIntro.kt
+++ My First Kotlin Application/Sandbox/src/TypeIntro.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>){
-    val playerName :String = "addick"
-    var experiencePoints: Int = 5
+    val playerName = "addick"
+    var experiencePoints = 5
     experiencePoints += 5
     println(experiencePoints)
     println(playerName)
