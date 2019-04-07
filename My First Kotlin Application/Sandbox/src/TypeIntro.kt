f539f57076bf892217cd93f09a1dc2cc731d5fe5
EDGAR LAPTOP-LENOVO <addickto@hotmail.com>
2019-04-07 11:44:15 -0500

    2.11 Challenge hasSteed

--- My First Kotlin Application/Sandbox/src/TypeIntro.kt
+++ My First Kotlin Application/Sandbox/src/TypeIntro.kt
@@ -1,6 +1,7 @@
 const val MAX_EXPERIENCE: Int = 5000
 
 fun main(args: Array<String>){
+    var hasSpeed: String = ""
     val playerName = "addick"
     var experiencePoints = 5
     experiencePoints += 5
