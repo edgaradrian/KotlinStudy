5a0e70b65878f19aea7b68184b1c4d762ab8eb39
Edgar Adrian <adick.teddy@gmail.com>
2019-04-08 16:29:49 -0500

    3.5 Using logical operators in a conditional

--- Conditionals/src/Game.kt
+++ Conditionals/src/Game.kt
@@ -3,6 +3,13 @@ fun main(args: Array<String>) {
     val name = "addick"
     var healthPoints = 89
     val isBlessed = true
+    val isImmortal = false
+
+    if ( isBlessed && healthPoints > 50 || isImmortal) {
+        println("GREEN")
+    } else {
+        println("NONE")
+    }
 
     if (healthPoints == 100) {
         println(name + " is in excellent condition")
