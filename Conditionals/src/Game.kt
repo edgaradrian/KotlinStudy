47abe0ea15a7b822b1778e65263350656439c5c4
Edgar Adrian <adick.teddy@gmail.com>
2019-04-08 15:20:55 -0500

    3.2 Modifying healthPoints

--- Conditionals/src/Game.kt
+++ Conditionals/src/Game.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
 
     val name = "addick"
-    var healthPoints = 100
+    var healthPoints = 89
 
     if (healthPoints == 100) {
         println(name + " is in excellent condition")
