5f9d6cdb16383ac1478d19d794b157fadafb425b
Edgar Adrian <adick.teddy@gmail.com>
2019-04-08 16:12:55 -0500

    3.4 Checking for blessedness

--- Conditionals/src/Game.kt
+++ Conditionals/src/Game.kt
@@ -2,13 +2,18 @@ fun main(args: Array<String>) {
 
     val name = "addick"
     var healthPoints = 89
+    val isBlessed = true
 
     if (healthPoints == 100) {
         println(name + " is in excellent condition")
     } else if (healthPoints >= 90) {
         println(name + " has a few scratches.")
     } else if (healthPoints >= 75) {
-        println(name + " has some minor wounds")
+        if (isBlessed) {
+            println(name + " has some minor wounds but is healing quite quickly!")
+        } else {
+            println(name + " has some minor wounds.")
+        }
     } else if (healthPoints >= 15) {
         println(name + " looks pretty hurt.")
     } else {
