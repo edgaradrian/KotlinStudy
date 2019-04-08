1fc4227f2b51692b5bbc6907a88232d6f3d469d3
Edgar Adrian <adick.teddy@gmail.com>
2019-04-08 14:17:08 -0500

    3.11 Printing the player's health condition

---
+++ Conditionals/src/Game.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+
+    val name = "addick"
+    var healthPoints = 100
+
+    if (healthPoints == 100) {
+        println(name + " is in excellent condition")
+    } else {
+        println(name + " is in awful condition")
+    }//else
+
+}//main
