b76a45ae4db92bb95c572588dc3c15e59f0d0d57
EDGAR LAPTOP-LENOVO <addickto@hotmail.com>
2019-04-10 14:46:59 -0500

    Challenge Enhancing the Aura

---
+++ Conditionals/NyetHack_ConditionalsChallenges/src/Game.kt
@@ -0,0 +1,36 @@
+fun main(args: Array<String>) {
+
+    val name = "addick"
+    var healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+
+    //Challenge
+    val karma = (Math.pow(Math.random(), (110 - healthPoints) / 100.0) * 20).toInt()
+
+    val auraColor = when (karma) {
+        in 0..5 -> "red"
+        in 6..10 -> "orange"
+        in 11..15 -> "purple"
+        in 16..20 -> "green"
+        else -> "gray"
+    }//auraColor
+
+    val healthStatus = when (healthPoints) {
+        100 -> "is in excellent condition"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
+                        "has some minor wounds but is healing quite quickly!"
+                     } else {
+                        "has some minor wounds."
+                     }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition"
+    }
+
+    println("(Aura: $auraColor)" +
+            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
+
+}//main
