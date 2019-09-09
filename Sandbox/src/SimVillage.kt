164536424a02d2570590219ee1d5dc3ac9d9878b
Edgar Adrián <adick.teddy@gmail.com>
2019-09-08 23:18:45 -0500

    Using the it keyword

--- Sandbox/src/SimVillage.kt
+++ Sandbox/src/SimVillage.kt
@@ -13,10 +13,18 @@ fun main(args: Array<String>) {
     println(greetingFunction())
 
     val otherGreetingFunction: (String) -> String = { player ->
-        val currenteYear = 2019
-        "Welcome to SimVillage, $player (copyright $currenteYear)"
+        val currentYear = 2019
+        "Welcome to SimVillage, $player (copyright $currentYear)"
     }//otherGreetingFunction
 
     println(otherGreetingFunction("addick"))
+
+    val itGreetingFunction: (String) -> String = {
+        val currentYear = 2019
+        "Welcome to SimVillage, $it (copyright $currentYear)"
+    }//itGreetingFunction
+
+    println(itGreetingFunction("addickted"))
+
 
 }//main
