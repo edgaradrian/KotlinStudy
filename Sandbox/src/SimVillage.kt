8d13661c4e4d0924acc44b4780106f018aa82661
Edgar Adrián <adick.teddy@gmail.com>
2019-09-08 22:39:57 -0500

    Assigning the anonymous function to a variable

--- Sandbox/src/SimVillage.kt
+++ Sandbox/src/SimVillage.kt
@@ -1,6 +1,15 @@
 fun main(args: Array<String>) {
+
     println({
         val currentYear = 2019
         "Welcome to SimVillage, Mayor (copyright $currentYear)"
     }())
+
+    val greetingFunction: () -> String = {
+        val currentYear = 2019
+        "Welcome to SimVillage, Mayor (copyright $currentYear)"
+    }
+
+    println(greetingFunction())
+
 }//main
