bdee43208ad02d1cdc7ea016ce4040417914d985
Edgar Adrián <adick.teddy@gmail.com>
2020-05-11 21:18:05 -0500

    Using optional single-expression function syntax (Game.kt)

--- Conditionals/src/Game.kt
+++ Conditionals/src/Game.kt
@@ -35,12 +35,12 @@ private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean
     return auraColor
 }//auraColor
 
-private fun castFireball(numFireballs: Int = 2) {
+private fun castFireball(numFireballs: Int = 2) =
     println("A glass of Fireball springs into existence. (x$numFireballs)")
-}//castFireball
 
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
-    val healthStatus = when (healthPoints) {
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
+    when (healthPoints) {
         100 -> "is in excellent condition"
         in 90..99 -> "has a few scratches."
         in 75..89 -> if (isBlessed) {
@@ -51,5 +51,3 @@ private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition"
     }
-    return healthStatus
-}//formatHealthStatus
