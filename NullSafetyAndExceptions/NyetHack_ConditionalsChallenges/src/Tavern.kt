de15bfe7f881213b5f7e748ab604ad935b803181
Edgar Adrián <adick.teddy@gmail.com>
2020-05-14 16:29:36 -0500

    Using the double-bang operator

--- NullSafetyAndExceptions/NyetHack_ConditionalsChallenges/src/Tavern.kt
+++ NullSafetyAndExceptions/NyetHack_ConditionalsChallenges/src/Tavern.kt
@@ -2,13 +2,14 @@ fun main(args: Array<String>) {
     //var signatureDrink = "Buttered Ate"
     //signatureDrink = null
     //var beverage = readLine()?.capitalize()
-    var beverage = readLine()?.let {
+    /*var beverage = readLine()?.let {
         if (it.isNotBlank()) {
             it.capitalize()
         } else {
             "Chela"
         }
-    }
+    }*/
+    var beverage = readLine()!!.capitalize()
 //    beverage = null
     println(beverage)
 }//main
