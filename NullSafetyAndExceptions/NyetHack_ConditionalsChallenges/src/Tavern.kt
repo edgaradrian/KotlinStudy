3987c1db7de16f39c5184d6ee007211060c3b96f
Edgar Adrián <adick.teddy@gmail.com>
2020-05-14 13:12:36 -0500

    Defining a nullable variable (Tavern.kt)

--- NullSafetyAndExceptions/NyetHack_ConditionalsChallenges/src/Tavern.kt
+++ NullSafetyAndExceptions/NyetHack_ConditionalsChallenges/src/Tavern.kt
@@ -1,4 +1,6 @@
 fun main(args: Array<String>) {
-    var signatureDrink = "Buttered Ate"
-    signatureDrink = null
+    //var signatureDrink = "Buttered Ate"
+    //signatureDrink = null
+    var beverage = readLine()
+    println(beverage)
 }//main
