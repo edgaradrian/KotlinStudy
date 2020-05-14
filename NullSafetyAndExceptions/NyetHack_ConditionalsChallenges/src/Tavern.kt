8847874b022a491cceb7f6667dcb7ca0ee06b37f
Edgar Adrián <adick.teddy@gmail.com>
2020-05-14 16:25:07 -0500

    Using let with the safe call operator

--- NullSafetyAndExceptions/NyetHack_ConditionalsChallenges/src/Tavern.kt
+++ NullSafetyAndExceptions/NyetHack_ConditionalsChallenges/src/Tavern.kt
@@ -1,7 +1,14 @@
 fun main(args: Array<String>) {
     //var signatureDrink = "Buttered Ate"
     //signatureDrink = null
-    var beverage = readLine()?.capitalize()
+    //var beverage = readLine()?.capitalize()
+    var beverage = readLine()?.let {
+        if (it.isNotBlank()) {
+            it.capitalize()
+        } else {
+            "Chela"
+        }
+    }
 //    beverage = null
     println(beverage)
 }//main
