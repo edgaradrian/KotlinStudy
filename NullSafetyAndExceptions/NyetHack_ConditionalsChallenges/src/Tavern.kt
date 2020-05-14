8c27833d60e7314956adbf880e8d77f9cc64c937
Edgar Adrián <adick.teddy@gmail.com>
2020-05-14 13:15:37 -0500

    Reassign a variable to null (Tavern.kt)

--- NullSafetyAndExceptions/NyetHack_ConditionalsChallenges/src/Tavern.kt
+++ NullSafetyAndExceptions/NyetHack_ConditionalsChallenges/src/Tavern.kt
@@ -2,5 +2,6 @@ fun main(args: Array<String>) {
     //var signatureDrink = "Buttered Ate"
     //signatureDrink = null
     var beverage = readLine()
+    beverage = null
     println(beverage)
 }//main
