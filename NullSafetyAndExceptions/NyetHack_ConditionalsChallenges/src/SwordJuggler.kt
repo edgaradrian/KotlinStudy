e01ab6d02d402384b376a11ad516aeb743906ad2
Edgar Adrián <adick.teddy@gmail.com>
2020-05-15 11:01:01 -0500

    Throwing and IllegalStateException (SwordJuggler.kt)

--- NullSafetyAndExceptions/NyetHack_ConditionalsChallenges/src/SwordJuggler.kt
+++ NullSafetyAndExceptions/NyetHack_ConditionalsChallenges/src/SwordJuggler.kt
@@ -1,3 +1,5 @@
+import java.lang.IllegalStateException
+
 fun main(args: Array<String>) {
     var swordJuggling: Int? = null
     val isJugglingProficent = (1..3).shuffled().last() == 3
@@ -5,7 +7,12 @@ fun main(args: Array<String>) {
         swordJuggling = 2
     }
 
+    proficiencyCheck(swordJuggling)
     swordJuggling = swordJuggling!!.plus(1)
 
     println("You juggle $swordJuggling sword!")
 }//main
+
+fun proficiencyCheck(swordsJuggling: Int?) {
+    swordsJuggling ?: throw IllegalStateException("Player cannot juggle swords")
+}//proficiencyCheck
