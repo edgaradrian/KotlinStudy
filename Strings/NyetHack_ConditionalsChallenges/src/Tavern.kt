d0ffac9e3b9dbf9715436923e52521c2338e8063
Edgar Adrián <adick.teddy@gmail.com>
2020-05-16 17:54:32 -0500

    Splitting the menu data

--- Strings/NyetHack_ConditionalsChallenges/src/Tavern.kt
+++ Strings/NyetHack_ConditionalsChallenges/src/Tavern.kt
@@ -1,11 +1,18 @@
 const val TAVERN_NAME = "Lobohombo's Folly"
 
 fun main(args: Array<String>) {
-    placeOrder()
+    placeOrder("shandy,Dragon's Breath,5.91")
 }//main
 
-private fun placeOrder() {
+private fun placeOrder(menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf("\'")
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("Dulce speaks with $tavernMaster about their order.")
+
+    val data = menuData.split(',')
+    val type = data[0]
+    val name = data[1]
+    val price = data[2]
+    val message = "Dulce buys a $name ($type) for $price."
+    println(message)
 }//place order
