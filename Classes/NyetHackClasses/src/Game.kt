ab4acac75d6a8ecb0772fd3985b7d0624fec4a36
Edgar Adrián <adick.teddy@gmail.com>
2020-05-21 13:07:47 -0500

    Adding class functions to Player (Player.kt)

--- Classes/NyetHackClasses/src/Game.kt
+++ Classes/NyetHackClasses/src/Game.kt
@@ -1,8 +1,6 @@
 fun main(args: Array<String>) {
 
-    var healthPoints = (Math.random() * 100).toInt()
-    val isBlessed = true
-    val isImmortal = false
+
     val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
 
     val player = Player()
@@ -33,20 +31,5 @@ private fun printPlayerStatus(
     println("$name $healthStatus")
 }//printPlayerStatus
 
-private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
-    return auraColor
-}//auraColor
 
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String  = when (healthPoints) {
-    100 -> "is in excellent condition"
-    in 90..99 -> "has a few scratches."
-    in 75..89 -> if (isBlessed) {
-        "has some minor wounds but is healing quite quickly!"
-    } else {
-        "has some minor wounds."
-    }
-    in 15..74 -> "looks pretty hurt."
-    else -> "is in awful condition"
-}
+
--- Classes/NyetHackClasses/src/Player.kt
+++ Classes/NyetHackClasses/src/Player.kt
@@ -9,5 +9,23 @@ class Player {
     val isBlessed = true
     private val isImmortal = false
 
+    private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+        val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+        val auraColor = if (auraVisible) "GREEN" else "NONE"
+        return auraColor
+    }//auraColor
+
+    private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String  = when (healthPoints) {
+        100 -> "is in excellent condition"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds but is healing quite quickly!"
+        } else {
+            "has some minor wounds."
+        }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition"
+    }
+
     fun castFireball(numFireballs: Int = 2) = println("A glass of Fireball springs into existence. (x$numFireballs)")
 }//Player
