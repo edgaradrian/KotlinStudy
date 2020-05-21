fee0f5f5e2c0b73953b6172458247255e303c61b
Edgar Adrián <adick.teddy@gmail.com>
2020-05-21 12:54:35 -0500

    Adding properties to Player(Player.kt)

--- Classes/NyetHackClasses/src/Player.kt
+++ Classes/NyetHackClasses/src/Player.kt
@@ -5,5 +5,9 @@ class Player {
             field = value.trim()
         }
 
+    var healthPoints = 90
+    val isBlessed = true
+    val isImmortal = false
+
     fun castFireball(numFireballs: Int = 2) = println("A glass of Fireball springs into existence. (x$numFireballs)")
 }//Player
