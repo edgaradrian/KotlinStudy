6eb6cf2114f672e646b89396c83693850b0a94d3
Edgar Adrián <adick.teddy@gmail.com>
2020-05-22 13:51:47 -0500

    Reassigning name

--- Classes/NyetHackClasses/src/com/edgaradrian/mx/Game.kt
+++ Classes/NyetHackClasses/src/com/edgaradrian/mx/Game.kt
@@ -20,6 +20,9 @@ fun main(args: Array<String>) {
     val sword = Sword("Excalibur")
     println(sword.name)
 
+    sword.name = "Gleipnir"
+    println(sword.name)
+
 }//com.edgaradrian.mx.main
 
 private fun printPlayerStatus(
