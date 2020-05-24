e56bfacb4b92fd722a64b367488fdfa632672ca4
Edgar Adrián <adick.teddy@gmail.com>
2020-05-24 16:29:46 -0500

    Adding new property and function to a subclass (Room.kt)

--- Inheritance/NyetHackInheritance/src/com/edgaradrian/mx/Room.kt
+++ Inheritance/NyetHackInheritance/src/com/edgaradrian/mx/Room.kt
@@ -12,6 +12,10 @@ open class Room(val name: String) {
 
 class TownSquare : Room("Town Square") {
     override val dangerLevel = super.dangerLevel - 3
-    override fun load() = "The villagers rally and cheer as you enter"
-}
+    private var bellSound = "GWONG"
+
+    override fun load() = "The villagers rally and cheer as you enter!\n${ringBell()}"
+
+    private fun ringBell() = "The bell tower announces your arrival. $bellSound"
+}//TownSquare
 
