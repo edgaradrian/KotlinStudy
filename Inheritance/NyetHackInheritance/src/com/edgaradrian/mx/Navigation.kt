7f0c5e1311866629912eae42b518b0643de2a11c
Edgar Adrián <adick.teddy@gmail.com>
2020-05-25 14:51:19 -0500

    Overloading the plus operator (Navigation.kt)

--- Inheritance/NyetHackInheritance/src/com/edgaradrian/mx/Navigation.kt
+++ Inheritance/NyetHackInheritance/src/com/edgaradrian/mx/Navigation.kt
@@ -12,4 +12,7 @@ enum class Direction(private val coordinate: Coordinate) {
 
 data class Coordinate(val x: Int, val y: Int) {
     val isInBounds = x >= 0 && y >= 0
+
+    operator fun plus(other: Coordinate) = Coordinate(x + other.x, y + other.y)
+
 }//Coordinate
