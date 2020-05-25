6ca09aeb1d7bc5de8691d156d2d598815983422a
Edgar Adrián <adick.teddy@gmail.com>
2020-05-25 14:12:41 -0500

    Defining a function in an enum (Navigation.kt)

--- Inheritance/NyetHackInheritance/src/com/edgaradrian/mx/Navigation.kt
+++ Inheritance/NyetHackInheritance/src/com/edgaradrian/mx/Navigation.kt
@@ -5,6 +5,9 @@ enum class Direction(private val coordinate: Coordinate) {
     EAST(Coordinate(1,0)),
     SOUTH(Coordinate(0,1)),
     WEST(Coordinate(-1,0))
+
+    fun updateCoordinate(playerCoordinate: Coordinate) =
+        Coordinate(playerCoordinate.x + coordinate.x, playerCoordinate.y + coordinate.y)
 }//Direction
 
 data class Coordinate(val x: Int, val y: Int) {
