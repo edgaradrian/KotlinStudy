0fe543f2df13bca213c326ae9948ecedfc254831
Edgar Adrián <adick.teddy@gmail.com>
2020-05-25 13:49:19 -0500

    Defining an enum (Navigation.kt)

--- Inheritance/NyetHackInheritance/src/com/edgaradrian/mx/Navigation.kt
+++ Inheritance/NyetHackInheritance/src/com/edgaradrian/mx/Navigation.kt
@@ -1,5 +1,12 @@
 package com.edgaradrian.mx
 
+enum class Direction {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+}//Direction
+
 data class Coordinate(val x: Int, val y: Int) {
     val isInBounds = x >= 0 && y >= 0
 }//Coordinate
