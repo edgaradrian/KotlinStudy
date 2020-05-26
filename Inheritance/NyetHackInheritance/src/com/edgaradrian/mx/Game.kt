02e448490122f450f65d763349b5b15edfb294da
Edgar Adrián <adick.teddy@gmail.com>
2020-05-26 18:28:24 -0500

    Defining the fight function (Game.kt)

--- Inheritance/NyetHackInheritance/src/com/edgaradrian/mx/Game.kt
+++ Inheritance/NyetHackInheritance/src/com/edgaradrian/mx/Game.kt
@@ -100,6 +100,13 @@ object Game {
             "Invalid direction: $directionInput."
         }
 
+    private fun fight() = currentRoom.monster?.let {
+        while (player.healthPoints > 0 && it.healthPoints > 0) {
+            Thread.sleep(1000)
+        }
+
+        "Combat complete."
+    } ?: "There's nothing here to fight"
 
 }//object Game
 
