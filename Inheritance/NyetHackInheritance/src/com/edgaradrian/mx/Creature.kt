69548cb3d652fa1f253b8ebfc0c736b297c7716c
Edgar Adrián <adick.teddy@gmail.com>
2020-05-26 17:20:21 -0500

    Defining an abstract class (Creature.kt)

--- Inheritance/NyetHackInheritance/src/com/edgaradrian/mx/Creature.kt
+++ Inheritance/NyetHackInheritance/src/com/edgaradrian/mx/Creature.kt
@@ -13,3 +13,16 @@ interface Fightable {
 
     fun attack(opponent: Fightable): Int
 }//Fightable
+
+abstract class Monster(val name: String,
+                       val description: String,
+                       override var healthPoints: Int
+                        ) : Fightable {
+
+    override fun attack(opponent: Fightable): Int {
+        val damageDealt = damageRoll
+        opponent.healthPoints -= damageDealt
+        return damageDealt
+    }//attack
+
+}//Monster
