bcffbaed5536892b747b6c71b11c69d70b8a4e3a
Edgar Adrián <adick.teddy@gmail.com>
2020-05-27 16:44:05 -0500

    Marking Barrel with in (Variance.kt)

--- Sandbox/src/Variance.kt
+++ Sandbox/src/Variance.kt
@@ -1,4 +1,4 @@
-class Barrel<out T>(val item: T)
+class Barrel<in T>(item: T)
 
 fun main(args: Array<String>) {
     var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("a generic-looking fedora", 15))
