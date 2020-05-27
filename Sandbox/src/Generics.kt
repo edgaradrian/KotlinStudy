a79510b6368c737e4617ffb3a7cd5bcfc3986ce8
Edgar Adrián <adick.teddy@gmail.com>
2020-05-27 13:47:29 -0500

    Testing the generic fetch function (Generics.kt)

--- Sandbox/src/Generics.kt
+++ Sandbox/src/Generics.kt
@@ -14,4 +14,9 @@ class Coin(val value: Int)
 fun main(args: Array<String>) {
     val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora", 15))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
+
+    lootBoxOne.fetch()?.run {
+        println("You retrieve a $name from the box!")
+    }
+
 }//main
