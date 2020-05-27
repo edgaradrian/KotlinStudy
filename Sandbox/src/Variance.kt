a25b598f9a5935ca40f5092244ecfb8010eeb69a
Edgar Adrián <adick.teddy@gmail.com>
2020-05-27 16:36:40 -0500

    Changing the assignment (Variance.kt)

--- Sandbox/src/Variance.kt
+++ Sandbox/src/Variance.kt
@@ -1,9 +1,9 @@
-class Barrel<out T>(var item: T)
+class Barrel<out T>(val item: T)
 
 fun main(args: Array<String>) {
     var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("a generic-looking fedora", 15))
     var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
 
-    lootBarrel.item = Coin(15)
-    val myFedora: Fedora = fedoraBarrel.item
+    lootBarrel = fedoraBarrel
+    val myFedora: Fedora = lootBarrel.item
 }//main
