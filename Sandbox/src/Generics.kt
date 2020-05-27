0d3680559a4f665e7d01a98c11307821536c6c2b
Edgar Adrián <adick.teddy@gmail.com>
2020-05-27 15:23:34 -0500

    Testing the new LootBox(Generics.kt)

--- Sandbox/src/Generics.kt
+++ Sandbox/src/Generics.kt
@@ -19,15 +19,16 @@ class Fedora(val name: String, value: Int) : Loot(value)
 class Coin(value: Int) : Loot(value)
 
 fun main(args: Array<String>) {
-    val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora", 15))
+    val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora", 15),
+                                              Fedora("a dazzling magenta fedora", 25))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
 
     lootBoxOne.open = true
-    lootBoxOne.fetch()?.run {
+    lootBoxOne.fetch(0)?.run {
         println("You retrieve a $name from the box!")
     }
 
-    val coin = lootBoxOne.fetch {
+    val coin = lootBoxOne.fetch(0) {
         Coin(it.value * 3)
     }
     coin?.let { println(it.value) }
