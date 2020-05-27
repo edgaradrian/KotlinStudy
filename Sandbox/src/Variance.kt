61a4de7cfad37f9f3725f6f51e5684d7610043f2
Edgar Adrián <adick.teddy@gmail.com>
2020-05-27 16:12:22 -0500

    Defining Barrels in main (Variance.kt)

---
+++ Sandbox/src/Variance.kt
@@ -0,0 +1,6 @@
+class Barrel<T>(var item: T)
+
+fun main(args: Array<String>) {
+    var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("a generic-looking fedora", 15))
+    var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
+}
