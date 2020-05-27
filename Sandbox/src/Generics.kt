4b0c6793a270c2e9dbb85c7077231c9ed987087d
Edgar Adrián <adick.teddy@gmail.com>
2020-05-27 13:52:31 -0500

    Opening the box (Generics.kt)

--- Sandbox/src/Generics.kt
+++ Sandbox/src/Generics.kt
@@ -15,6 +15,7 @@ fun main(args: Array<String>) {
     val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora", 15))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
 
+    lootBoxOne.open = true
     lootBoxOne.fetch()?.run {
         println("You retrieve a $name from the box!")
     }
