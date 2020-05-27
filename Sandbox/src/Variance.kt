72e4e79df99498416860bce605f14d673d05d03a
Edgar Adrián <adick.teddy@gmail.com>
2020-05-27 16:33:42 -0500

    Adding out (Variance.kt)

--- Sandbox/src/Variance.kt
+++ Sandbox/src/Variance.kt
@@ -1,8 +1,9 @@
-class Barrel<T>(var item: T)
+class Barrel<out T>(var item: T)
 
 fun main(args: Array<String>) {
     var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("a generic-looking fedora", 15))
     var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
 
     lootBarrel.item = Coin(15)
+    val myFedora: Fedora = fedoraBarrel.item
 }//main
