c0ce833b9ddf3a4c82d64466cb31805e4502fd16
Edgar Adrián <adick.teddy@gmail.com>
2020-05-27 16:23:00 -0500

    Assigning a coin to lootBarrel.item (Variance.kt)

--- Sandbox/src/Variance.kt
+++ Sandbox/src/Variance.kt
@@ -3,4 +3,6 @@ class Barrel<T>(var item: T)
 fun main(args: Array<String>) {
     var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("a generic-looking fedora", 15))
     var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
-}
+
+    lootBarrel.item = Coin(15)
+}//main
