95d71356dbdc4aa3fad8af97aad9aaae1fbc4ce2
Edgar Adrián <adick.teddy@gmail.com>
2020-05-28 11:09:56 -0500

    Making easyPrint chainable (Extensions.kt)

--- Sandbox/src/Extensions.kt
+++ Sandbox/src/Extensions.kt
@@ -1,6 +1,9 @@
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
-fun Any.easyPrint() = println(this)
+fun Any.easyPrint(): Any {
+    println(this)
+    return this
+}
 
 fun main(args: Array<String>) {
     "Edgar has left the building".addEnthusiasm().easyPrint()
