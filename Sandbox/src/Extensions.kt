0de3b0611a76b3c4619aa5ca501d781cb73a89bb
Edgar Adrián <adick.teddy@gmail.com>
2020-05-28 11:01:17 -0500

    Extending Any (Extensions.kt)

--- Sandbox/src/Extensions.kt
+++ Sandbox/src/Extensions.kt
@@ -1,6 +1,8 @@
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
+fun Any.easyPrint() = println(this)
+
 fun main(args: Array<String>) {
-    println("Edgar has left the building".addEnthusiasm())
+    "Edgar has left the building".addEnthusiasm().easyPrint()
 }//main
 
