4a8454f917500fead0bb195db3e144bd61697d5b
Edgar Adrián <adick.teddy@gmail.com>
2020-05-31 17:06:35 -0500

    Returning null from a java method (Jhava.java)

--- Interoperability/Interop/src/Jhava.java
+++ Interoperability/Interop/src/Jhava.java
@@ -2,6 +2,10 @@ public class Jhava {
 
     public String utterGreeting() {
         return "BLARGH";
+    }//utterGreeting
+
+    public String determineFriendshipLevel() {
+        return null;
     }
 
 }
