0e0de092e87cd8b22036c82bca1e29a913f1b313
Edgar Adrián <adick.teddy@gmail.com>
2020-06-01 22:00:09 -0500

    Specifying that a return value will not be null (Jhava.java)

--- Interoperability/Interop/src/Jhava.java
+++ Interoperability/Interop/src/Jhava.java
@@ -1,7 +1,9 @@
+import com.sun.istack.internal.NotNull;
 import com.sun.istack.internal.Nullable;
 
 public class Jhava {
 
+    @NotNull
     public String utterGreeting() {
         return "BLARGH";
     }//utterGreeting
