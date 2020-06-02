2d684864cea743a2b949983f489fa0b14df5f45e
Edgar Adrián <adick.teddy@gmail.com>
2020-06-01 21:41:17 -0500

    Specifying that a return value will possibly be null (Jhava.java)

--- Interoperability/Interop/src/Jhava.java
+++ Interoperability/Interop/src/Jhava.java
@@ -1,9 +1,12 @@
+import com.sun.istack.internal.Nullable;
+
 public class Jhava {
 
     public String utterGreeting() {
         return "BLARGH";
     }//utterGreeting
 
+    @Nullable
     public String determineFriendshipLevel() {
         return null;
     }
