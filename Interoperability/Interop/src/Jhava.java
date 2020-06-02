8d9d810b7a0e35f4902c759120bb5bf485f699be
Edgar Adrián <adick.teddy@gmail.com>
2020-06-01 22:09:02 -0500

    Declaring an int in Java (Jhava.java)

--- Interoperability/Interop/src/Jhava.java
+++ Interoperability/Interop/src/Jhava.java
@@ -3,6 +3,8 @@ import com.sun.istack.internal.Nullable;
 
 public class Jhava {
 
+    public int hitPoints = 51999004;
+
     @NotNull
     public String utterGreeting() {
         return "BLARGH";
