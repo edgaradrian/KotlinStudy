dc61fc99efdeb4d18d4fa943542540a50fb98d5a
Edgar Adrián <adick.teddy@gmail.com>
2022-10-20 16:29:10 -0500

    Challenge 1  -  hasDogs variable (Main.kt)

---
+++ 2ed2021/Chapter2/Challenges/MyFirstKotlinProject/src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+
+const val NOMBRE = "Edgar Adrián"
+fun main() {
+    println("Inicia el nivel")
+
+    println(NOMBRE)
+
+    var level = 3
+    println(level)
+
+    println("Subiendo de nivel")
+    level += 1
+    println(level)
+
+    /* Challenge 1  -  hasDogs variable */
+    var hasDogs = 0
+    println("Tiene " + hasDogs + " perros")
+    hasDogs += 1
+    println("Ahora tiene " + hasDogs + " perro")
+
+}//main
