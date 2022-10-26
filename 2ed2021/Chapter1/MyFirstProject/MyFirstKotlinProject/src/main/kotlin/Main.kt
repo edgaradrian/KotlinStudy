a93a6794b26d966f2d1ccd276b16d4bed0600a8d
Edgar Adrián <adick.teddy@gmail.com>
2022-10-26 15:17:03 -0500

    Printing the player’s quest (Main.kt)

--- 2ed2021/Chapter1/MyFirstProject/MyFirstKotlinProject/src/main/kotlin/Main.kt
+++ 2ed2021/Chapter1/MyFirstProject/MyFirstKotlinProject/src/main/kotlin/Main.kt
@@ -8,6 +8,12 @@ fun main() {
     var level = 3
     println(level)
 
+    if (level == 1) {
+        println("Está en el nivel uno")
+    } else {
+        println("Ha superado el nivel 1")
+    }
+
     println("Subiendo de nivel")
     level += 1
     println(level)
