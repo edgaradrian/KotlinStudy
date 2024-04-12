d93cdcf68a6700d17fcfb6f33725c5a121456ea4
Edgar Adrián <ingeniero@tuta.com>
2024-04-11 20:31:17 -0600

    Using string concatenation (Main.kt)

--- SecondEdition/bounty-board/src/main/kotlin/Main.kt
+++ SecondEdition/bounty-board/src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ const val heroName = "addick"
 var playerLevel = 5
 
 fun main(args: Array<String>) {
-    println("The hero starts the game")
+    println(heroName + " starts the game")
 
     println(heroName)
     println(playerLevel)
