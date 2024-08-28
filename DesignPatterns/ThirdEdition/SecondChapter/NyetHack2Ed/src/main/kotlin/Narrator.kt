014b5d8a6615789e4c2a1c929806352ab2de6977
Edgar Adrián <ingeniero@tuta.com>
2024-08-28 14:34:46 -0600

    feat(Cap8.10): Adding more narrator moods (Narrator.kt)

--- DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/Narrator.kt
+++ DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/Narrator.kt
@@ -1,10 +1,52 @@
-val narrationModifier =  { message: String ->
-    val numExclamationsPoints = 3
-    "¡" + message.uppercase() + "!".repeat(numExclamationsPoints)
-}
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+var narrationModifier: (String) -> String = { it }
 
 fun narrate(
     message: String
 ) {
     println(narrationModifier(message))
 }//narrate
+
+fun changeNarratorMood() {
+
+    val mood : String
+    val modifier:  (String) -> String
+
+    when (Random.nextInt(1..4)) {
+
+        1 -> {
+            mood = "loud"
+            modifier = { message ->
+                val numExclamationPoint = 3
+                "¡".repeat(numExclamationPoint) + message.uppercase() + "!".repeat(numExclamationPoint)
+            }
+        }
+
+        2 -> {
+            mood = "tired"
+            modifier = { message ->
+                message.lowercase().replace(" ", "... ")
+            }
+        }
+
+        3 -> {
+            mood = "unsure"
+            modifier = { message ->
+                "¿$message?"
+            }
+        }
+
+        else -> {
+            mood = "professional"
+            modifier = { message ->
+                "$message."
+            }
+        }
+    }
+
+        narrationModifier = modifier
+        narrate("El narrador está en modo: $mood")
+
+}//changeNarratorMood
