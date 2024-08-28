c1f9c97daf05cd85a8ec28bc77f4e5ba4390a5fd
Edgar Adrián <ingeniero@tuta.com>
2024-08-28 12:21:39 -0600

    feat(Cap8.6): Adding a message parameter (Narrator.kt)

--- DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/Narrator.kt
+++ DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/Narrator.kt
@@ -1,10 +1,10 @@
+val narrationModifier: (String) -> String =  { message ->
+    val numExclamationsPoints = 3
+    "¡" + message.uppercase() + "!".repeat(numExclamationsPoints)
+}
+
 fun narrate(
     message: String
 ) {
-    val narrationModifier: () -> String =  {
-        val numExclamationsPoints = 3
-        "¡" + message.uppercase() + "!".repeat(numExclamationsPoints)
-    }
-
-    println(narrationModifier)
+    println(narrationModifier(message))
 }//narrate
