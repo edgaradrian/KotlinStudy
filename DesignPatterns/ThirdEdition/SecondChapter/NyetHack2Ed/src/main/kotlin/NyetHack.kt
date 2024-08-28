948662e796cbdbce6b841c2521f593120a4f6d12
Edgar Adrián <ingeniero@tuta.com>
2024-08-28 14:37:12 -0600

    feat(Cap8.11): Changing the narrator’s mood (NyetHack.kt)

--- DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/NyetHack.kt
+++ DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/NyetHack.kt
@@ -3,5 +3,7 @@ fun main() {
     narrate("Un héroe entra al Distrito Federal. ¿Cuál es su nombre?")
     val heroName = readLine() ?: "Édgar"
 
+    changeNarratorMood()
+    narrate("$heroName llega al centro")
 }//main
 
