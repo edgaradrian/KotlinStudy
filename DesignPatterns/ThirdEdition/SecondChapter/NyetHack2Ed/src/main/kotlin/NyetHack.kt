643ee0c8cc7df93c864dd01e9c6d4fc6f4382f94
Edgar Adrián <ingeniero@tuta.com>
2024-08-28 16:35:11 -0600

    feat(Cap8.16): Remembering require (NyetHack.kt)

--- DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/NyetHack.kt
+++ DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/NyetHack.kt
@@ -3,7 +3,10 @@ fun main() {
     narrate("Un héroe entra al Distrito Federal. ¿Cuál es su nombre?") { message ->
         "\u001b[33;1m$message\u001b[0m"
     }
-    val heroName = readLine() ?: "Édgar"
+    val heroName = readLine()
+    require(heroName != null && heroName.isNotEmpty()) {
+        "El héroe debe tener un nombre"
+    }
 
     changeNarratorMood()
     narrate("$heroName llega al centro")
