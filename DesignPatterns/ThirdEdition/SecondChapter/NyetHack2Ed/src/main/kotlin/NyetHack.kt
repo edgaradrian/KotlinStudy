666966698813cc1db433f488b4ac94817b0b78e6
Edgar Adrián <ingeniero@tuta.com>
2024-08-28 16:25:14 -0600

    feat(Cap8.14): Passing a trailing lambda (NyetHack.kt)

--- DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/NyetHack.kt
+++ DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/NyetHack.kt
@@ -1,8 +1,8 @@
 fun main() {
 
-    narrate("Un héroe entra al Distrito Federal. ¿Cuál es su nombre?", { message ->
+    narrate("Un héroe entra al Distrito Federal. ¿Cuál es su nombre?") { message ->
         "\u001b[33;1m$message\u001b[0m"
-    })
+    }
     val heroName = readLine() ?: "Édgar"
 
     changeNarratorMood()
