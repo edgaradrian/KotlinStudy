4584077e444a5536c7115949b4654cae7dcf18f3
Edgar Adrián <ingeniero@tuta.com>
2024-08-28 11:03:13 -0600

    feat(Cap8.1) NyetHack’s humble beginning (NyetHack.kt)

--- DesignPatterns/ThirdEdition/SecondChapter/CreationalPatterns/src/main/kotlin/Main.kt
+++ DesignPatterns/ThirdEdition/SecondChapter/CreationalPatterns/src/main/kotlin/Main.kt
@@ -1,5 +1,39 @@
 package org.example
-object NoMovieList
+
+object NoMovieList : List<String> {//object NoMovieList
+    override val size: Int = 0
+    override fun get(index: Int): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun isEmpty(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun iterator(): Iterator<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun listIterator(): ListIterator<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun listIterator(index: Int): ListIterator<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun subList(fromIndex: Int, toIndex: Int): List<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun lastIndexOf(element: String): Int = 0
+
+    override fun indexOf(element: String): Int = 0
+
+    override fun containsAll(elements: Collection<String>) = false
+
+    override fun contains(element: String): Boolean = false
+}
 
 fun main() {
     println("Creational Patterns")
---
+++ DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/NyetHack.kt
@@ -0,0 +1,6 @@
+fun main() {
+
+    println("Un héroe entra al Distrito Federal. ¿Cuál es su nombre?")
+    val heroName = readLine() ?: "Édgar"
+
+}//main
