a87b888def92741a86aab23cc4f82fffeeb446ef
Edgar Adrián <ingeniero@tuta.com>
2024-09-05 17:42:56 -0600

    feat(Cap9.6): Accessing the first patron (Tavern.kt)

--- DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/Tavern.kt
+++ DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/Tavern.kt
@@ -4,7 +4,7 @@ private const val TAVERN_NAME = "Locura de $TAVERN_MASTER"
 fun visitTavern() {
     narrate("$heroName entra a $TAVERN_NAME")
 
-    val patrons: List<String> = listOf("Edgar", "Adrián", "Astrid")
-    println(patrons)
+    val patrons= listOf("Edgar", "Adrián", "Astrid")
+    println(patrons[0])
 
 }//visitTavern
