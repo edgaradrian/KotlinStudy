6249a3d446b811c18eced5cada5dfb60e7497410
Edgar Adrián <ingeniero@tuta.com>
2024-09-05 17:39:39 -0600

    feat(Cap9.3): Creating a list of patrons (Tavern.kt)

--- DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/Tavern.kt
+++ DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/Tavern.kt
@@ -2,5 +2,9 @@ private const val TAVERN_MASTER = "Bob"
 private const val TAVERN_NAME = "Locura de $TAVERN_MASTER"
 
 fun visitTavern() {
+    narrate("$heroName entra a $TAVERN_NAME")
+
+    val patrons: List<String> = listOf("Edgar", "Adrián", "Astrid")
+    println(patrons)
 
 }//visitTavern
