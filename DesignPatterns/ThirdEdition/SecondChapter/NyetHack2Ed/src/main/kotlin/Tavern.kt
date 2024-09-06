6d6b33ed280d29bafdc6a5126123112a0e51f9ad
Edgar Adrián <ingeniero@tuta.com>
2024-09-05 18:04:39 -0600

    feat(Cap9.12): Making the patron list mutable (Tavern.kt)

--- DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/Tavern.kt
+++ DesignPatterns/ThirdEdition/SecondChapter/NyetHack2Ed/src/main/kotlin/Tavern.kt
@@ -4,7 +4,7 @@ private const val TAVERN_NAME = "Locura de $TAVERN_MASTER"
 fun visitTavern() {
     narrate("$heroName entra a $TAVERN_NAME")
 
-    val patrons= listOf("Edgar", "Adrián", "Astrid")
+    val patrons= mutableListOf("Edgar", "Adrián", "Astrid")
 
     val astridMessage = if (patrons.contains("Astrid")) {
         "$TAVERN_MASTER dice: Astrid está atrás jugando cartas"
@@ -21,5 +21,11 @@ fun visitTavern() {
     }
 
     println(otherMessage)
+
+    narrate("Astrid deja la Taverna $TAVERN_NAME")
+    patrons.remove("Astrid")
+    narrate("Abril entra a la Taverna $TAVERN_NAME")
+    patrons.add("Abril")
+    println(patrons)
 
 }//visitTavern
